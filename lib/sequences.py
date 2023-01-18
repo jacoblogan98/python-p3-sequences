422fdf4d970cfbf96dd4b81ad607384c5a53d39e
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-01-17 21:34:33 -0500

    passed tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = 0
+    current = 0
+    fib_list = []
+
+    while i < length:
+        fib_list.append(current)
+        if current == 0:
+            current = 1
+        else: 
+            current = current + fib_list[i - 1]
+        i += 1
+    
+    print(fib_list)
